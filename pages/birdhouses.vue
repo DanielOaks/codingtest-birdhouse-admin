76075c28d214b72419a09afe793e35ba1b89a21d
Daniel Oaks <daniel@danieloaks.net>
2023-09-09 19:19:29 +1000

    Add test birdhouses page

---
+++ pages/birdhouses.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-grow flex-col items-center justify-center">
+    <h1 class="mb-7 text-2xl font-semibold">Home page</h1>
+    <NuxtLink to="/" class="rounded-lg bg-sbpurple px-5 py-3"> Back </NuxtLink>
+  </div>
+</template>
