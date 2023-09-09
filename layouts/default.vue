cbd8916a073a077d4e90c24cd02322e3c7f06cd0
Daniel Oaks <daniel@danieloaks.net>
2023-09-09 10:23:36 +1000

    Update favicon, use layouts

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Head>
+      <Link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <Link rel="manifest" href="/site.webmanifest" />
+      <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <Meta name="msapplication-TileColor" content="#1f1f1f" />
+      <Meta name="theme-color" content="#111111" />
+    </Head>
+
+    <Body class="bg-white text-black dark:bg-zinc-900 dark:text-white">
+      <TheHeader />
+      <slot />
+    </Body>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk
+      ? `${titleChunk} - Shockbyte BirdHouses Admin`
+      : "Shockbyte BirdHouses Admin";
+  },
+});
+</script>
