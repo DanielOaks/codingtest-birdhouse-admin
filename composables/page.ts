187d03a055284f3ab4a34a8a03ec0d8213798aa8
Daniel Oaks <daniel@danieloaks.net>
2023-09-13 13:45:20 +1000

    Fix a bug when getting initial ?page value

--- composables/page.ts
+++ composables/page.ts
@@ -6,10 +6,19 @@
 //  as sometimes Nuxt's router doesn't recognise the query string values when
 //  loading a new page from the back button
 export function useInitialPageNumber(): number {
+  const routerPage = useRoute().query.page?.toString();
+  const windowPage = new URLSearchParams(window.location.search)
+    .get("page")
+    ?.toString();
+
+  // when running navigateTo() to visit a new page, the router may still
+  //  contain the old page's query parameters in the <setup> function...
+  // we use this to check whether the router and window.location are in sync,
+  //  and if so we can rely on the router's query parameters as being accurate
+  const windowLocationMatches = useRoute().path === window.location.pathname;
+
   return parseInt(
-    useRoute().query.page?.toString() ||
-      new URLSearchParams(window.location.search).get("page")?.toString() ||
-      "1",
+    (windowLocationMatches ? routerPage : undefined) || windowPage || "1",
   );
 }
 
@@ -23,6 +32,7 @@ export function usePopStatePage(callback: newPageCallback) {
   const respondToNewPopState = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const newPage = parseInt(urlParams.get("page")?.toString() || "1");
+    console.log("new pop state with page", newPage);
     callback(newPage);
   };
 
